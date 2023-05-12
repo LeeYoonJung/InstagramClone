b823ffc5b1c446ab1ca5d6e0f1413556abd18dda
LeeYoonJung <imj1017@naver.com>
2023-05-12 16:50:33 +0900

    프로젝트 생성 및 최초 페이지 Tab Page 구현

---
+++ lib/tab/tabPage.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class TabPage extends StatefulWidget {
+  const TabPage({Key? key}) : super(key: key);
+
+  @override
+  State<TabPage> createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'home',
+          ),BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'search',
+          ),BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'account',
+          ),
+        ],
+      ),
+    );
+  }
+}
